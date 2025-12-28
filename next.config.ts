f006fef7226377d1a324623cf394dc2fbf1eae0d
shrage <shreyansh3536@gmail.com>
2025-12-28 22:51:26 +0545

    next config gix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -8,6 +9,10 @@ const nextConfig: NextConfig = {
         destination: "http://localhost:8005/api/auth/:path*",
       },
     ];
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
   devIndicators: false,
   images: {
